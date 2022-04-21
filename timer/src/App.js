f05d56a44a532455ba5a6e53120e7296fd3ff228
Jihyo Kang <luxuryjihyo@naver.com>
2022-04-21 18:53:45 +0900

    21 Apr 2022 2

--- timer/src/App.js
+++ timer/src/App.js
@@ -1,26 +1,79 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button, Table, Col } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <div>
+          Blind Structure
+        </div> 
+
+        
+
+        
+        <div className="container d-flex">
+          <Col sm={6}>
+            <div>
+              <input placeholder="Level"></input>
+              <input placeholder="Break level"></input>
+              <button>추가</button>
+            </div>
+          </Col>
+          <Col sm={6}>
+            <div>
+              <input placeholder="Title"></input>
+              <button>저장하기</button>
+            </div>
+            <Table responsive="sm">
+              <thead>
+                <tr>
+                  <th>Level</th>
+                  <th>Small</th>
+                  <th>Big</th>
+                  <th>Ante</th>
+                  <th>Time</th>
+
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1</td>
+                  <td><input></input></td>
+                  <td><input></input></td>
+                  <td><input></input></td>
+                  <td><input></input></td>
+
+                </tr>
+                <tr>
+                  <td>2</td>
+                  <td><input></input></td>
+                  <td><input></input></td>
+                  <td><input></input></td>
+                  <td><input></input></td>
+
+                </tr>
+                <tr>
+                  <td>3</td>
+                  <td><input></input></td>
+                  <td><input></input></td>
+                  <td><input></input></td>
+                  <td><input></input></td>
+                </tr>
+              </tbody>
+            </Table>
+          </Col>
+        </div>
     </div>
   );
 }
 
+
+
 export default App;
+
+function CreateStructure(){
+
+}
